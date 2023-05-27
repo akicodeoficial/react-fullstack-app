a17b31b666d3c30244d403b4e06910ecdaaea6e3
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-27 13:33:43 -0300

    criando a primeira rota

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  return res.json({ message: 'ok' })
+})
+
 app.listen(3333, () => {
   console.log('Servidor rodando na porta 3333')
 })
