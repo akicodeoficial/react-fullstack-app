a3919cd68bbcb904dc32ff53b0da7d3edb9d6367
Alan Silva <126093767+akicodeoficial@users.noreply.github.com>
2023-05-27 13:39:08 -0300

    Criando as rotas do servidor

---
+++ backend/src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const routes = express.Router()
+
+const user = {
+  email: 'akicodeoficial@gmail.com',
+  password: 'root'
+}
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'ok' })
+})
+
+routes.post('/login', (req, res) => {
+  const { email, password } = req.body
+
+  if (email === '' || password === '') {
+    return res.status(400).json({ message: 'Email e senha são obrigatórios' })
+  }
+
+  if (email !== user.email || password !== user.password) {
+    return res.status(401).json({ message: 'Email ou senha inválidos' })
+  }
+
+  return res.json({ message: 'ok' })
+})
+
+module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'ok' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Servidor rodando na porta 3333')
